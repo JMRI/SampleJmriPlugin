1f05efec0394ad230f8b5c60d7429fa66abcd4de
Daniel <db123@bergqvist.se>
2025-08-25 23:39:08 +0200

    Add files from https://github.com/danielb987/JmriPlugin

---
+++ src/se/bergqvist/jmri/example/Example.java
@@ -0,0 +1,17 @@
+package se.bergqvist.jmri.example;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Example plugin
+ *
+ * @author Daniel Bergqvist (C) 2023
+ */
+public class Example {
+
+    public void helloWorld() {
+        JOptionPane.showMessageDialog(null, "Hello World", "Plugin example",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+}
